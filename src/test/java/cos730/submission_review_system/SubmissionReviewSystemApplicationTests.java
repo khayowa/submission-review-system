330c52c5f005a5bb7ad03609060e8aaa04c9323f
DISCOVERY\KHAYELIH4 <khayelihlem@discovery.co.za>
2026-04-26 09:28:46 +0200

    Spring Boot project setup for COS730 Assignment 2

---
+++ src/test/java/cos730/submission_review_system/SubmissionReviewSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package cos730.submission_review_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmissionReviewSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
